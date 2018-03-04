dbbcf349a7a027ce80e11bd49da87ad1b2f815fb
dimas <dimasz_97@yahoo.com>
2018-03-05 04:55:18 +0700

    Migration Server to PT Server

--- app/src/main/java/bleizing/punyatemenuser/NetAPI.java
+++ app/src/main/java/bleizing/punyatemenuser/NetAPI.java
@@ -5,8 +5,8 @@ package bleizing.punyatemenuser;
  */
 
 public class NetAPI {
-    public static final String URL = "http://pt.himsigalaksi.com/index.php/";
-//    public static final String URL = "http://pt.punyatemen.com/index.php/";
+//    public static final String URL = "http://pt.himsigalaksi.com/index.php/";
+    public static final String URL = "http://pt.punyatemen.com/index.php/";
     public static final String GET_BARANG_SEWA = URL + "getBarangSewa/";
     public static final String GET_PERMINTAAN_BARANG_BY_CALON_PENYEWA_ID = URL + "getPermintaanBarangByCalonPenyewaId/";
 }
